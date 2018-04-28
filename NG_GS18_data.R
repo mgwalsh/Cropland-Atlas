f65b5cb3605680fc9cbaf02ac9d684254ed98347
mgwalsh <markusgwalsh@gmail.com>
2018-04-28 16:27:42 +0300

    Nigeria 2018 GeoSurvey 250m resolution cropland data setup

---
+++ NG_GS18_data.R
@@ -0,0 +1,94 @@
+# Nigeria 2018 GeoSurvey 250m resolution cropland data setup
+# M. Walsh, April 2018
+
+# Required packages
+# install.packages(c("downloader","rgdal","jsonlite","raster","leaflet","htmlwidgets","wordcloud")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(jsonlite)
+  require(raster)
+  require(leaflet)
+  require(htmlwidgets)
+  require(wordcloud)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("NG_GS18", showWarnings = F)
+setwd("./NG_GS18")
+
+# download GeoSurvey data
+# see sampling frame script @ https://github.com/mgwalsh/Sampling/blob/master/NG_GS_sample.R
+download("https://www.dropbox.com/s/adn9xdbvfj2cdo3/NG_geos_2018.csv.zip?raw=1", "NG_geos_2018.csv.zip", mode = "wb")
+unzip("NG_geos_2018.csv.zip", overwrite = T)
+geos <- read.table("NG_geos_2018.csv", header = T, sep = ",")
+geos$BIC <- as.factor(ifelse(geos$CP == "Y" & geos$BP == "Y", "Y", "N")) ## identifies croplands with buildings
+
+# download GADM-L2 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/y3h6l7yu00orm78/NGA_adm2.zip?raw=1", "NGA_adm2.zip", mode = "wb")
+unzip("NGA_adm2.zip", overwrite = T)
+shape <- shapefile("NGA_adm2.shp")
+
+# download raster stack (note this is a biggish 800+ Mb download, 38 rasters)
+download("https://www.dropbox.com/s/y1ht9tazxqvsggp/TZ_250m_2018.zip?raw=1", "TZ_250m_2018.zip", mode = "wb")
+unzip("NG_250m_2017.zip", overwrite = T)
+glist <- list.files(pattern="tif", full.names = T)
+grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L2 admin unit names from shape
+coordinates(geos) <- ~lon+lat
+projection(geos) <- projection(shape)
+gadm <- geos %over% shape
+geos <- as.data.frame(geos)
+geos <- cbind(gadm[ ,c(5,7)], geos)
+colnames(geos) <- c("state","lga","time","id","observer","lat","lon","BP","CP","WP","rice","cgrid","bloc","BIC")
+
+# Count number of buildings per quadrat -----------------------------------
+bp <- geos[which(geos$BP == "Y"), ] ## identify quadrats with buildings
+bp$bloc <- as.character(bp$bloc)
+
+# Counting tagged building locations from quadrats with buildings
+bcount <- rep(NA, nrow(bp))
+for(i in 1:nrow(bp)) {
+  t <- fromJSON(bp$bloc[i])
+  bcount[i] <- nrow(t$features)
+}
+bcount ## vector of number of buildings per quadrats with buildings
+ba <- geos[which(geos$BP == "N"), ]
+ba$bcount <- 0
+bp <- cbind(bp, bcount)
+geos <- rbind(ba, bp)
+geos <- geos[order(geos$id),] ## sort in original sample order
+
+# project GeoSurvey coords to grid CRS
+geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geos.proj) <- c("x","y")
+geos <- cbind(geos, geos.proj)
+coordinates(geos) <- ~x+y
+projection(geos) <- projection(grids)
+
+# extract gridded variables at GeoSurvey locations
+geosgrid <- extract(grids, geos)
+gsdat <- as.data.frame(cbind(geos, geosgrid)) 
+# gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates ... if needed
+gsdat <- gsdat[complete.cases(gsdat[ ,c(8:11)]),] ## removes incomplete cases
+# gsdat <- gsdat[ which(gsdat$CP=='Y'), ] ## selects croplands only
+gsdat$observer <- sub("@.*", "", as.character(gsdat$observer)) ## shortens observer ID's
+
+# Write data frame --------------------------------------------------------
+dir.create("Results", showWarnings = F)
+write.csv(gsdat, "./Results/NG_gsdat18.csv", row.names = F)
+
+# GeoSurvey map widget ----------------------------------------------------
+w <- leaflet() %>% 
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(gsdat$lon, gsdat$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'NG_GS18.html', selfcontained = T) ## save widget
+
+# GeoSurvey contributions -------------------------------------------------
+gscon <- as.data.frame(table(gsdat$observer))
+set.seed(1235813)
+wordcloud(gscon$Var1, freq = gscon$Freq, scale = c(3,0.1), random.order = T)
