545719c97a66c2fa2b6508e268067315a3c06340
mgwalsh <markusgwalsh@gmail.com>
2018-05-15 05:48:45 +0300

    Ghana 2018 GeoSurvey 250m resolution cropland data setup

---
+++ GH_GS18_data.R
@@ -0,0 +1,106 @@
+# Ghana 2018 GeoSurvey 250m resolution cropland data setup
+# M. Walsh, May 2018
+
+# Required packages
+# install.packages(c("downloader","rgdal","jsonlite","raster","leaflet","htmlwidgets","wordcloud")), dependencies=TRUE)
+suppressPackageStartupMessages({
+  require(downloader)
+  require(rgdal)
+  require(jsonlite)
+  require(raster)
+  require(leaflet)
+  require(htmlwidgets)
+  require(wordcloud)
+})
+
+# Data downloads -----------------------------------------------------------
+# set working directory
+dir.create("GH_GS18", showWarnings = F)
+setwd("./GH_GS18")
+
+# download GeoSurvey data
+download("https://www.dropbox.com/s/cdgz89n3vc97m3m/GH_GS_2018.csv.zip?raw=1", "GH_GS_2018.csv.zip", mode = "wb")
+unzip("GH_GS_2018.csv.zip", overwrite = T)
+geos <- read.table("GH_GS_2018.csv", header = T, sep = ",")
+geos$BIC <- as.factor(ifelse(geos$CP == "Y" & geos$BP == "Y", "Y", "N")) ## identifies croplands with buildings
+
+# download GADM-L2 shapefile (courtesy: http://www.gadm.org)
+download("https://www.dropbox.com/s/rnl5ixtzspbfwd3/GH_GADM_L2.zip?raw=1", "GH_GADM_L2.zip", mode = "wb")
+unzip("GH_GADM_L2.zip", overwrite = T)
+shape <- shapefile("GHA_adm2.shp")
+
+# download raster stack
+download("https://www.dropbox.com/s/jp32rvs8coca8lk/GH_250m_2017.zip?raw=1", "GH_250m_2017.zip", mode = "wb")
+unzip("GH_250m_2017.zip", overwrite = T)
+glist <- list.files(pattern="tif", full.names = T)
+grids <- stack(glist)
+
+# Data setup --------------------------------------------------------------
+# attach GADM-L2 admin unit names from shape
+coordinates(geos) <- ~lon+lat
+projection(geos) <- projection(shape)
+gadm <- geos %over% shape
+geos <- as.data.frame(geos)
+geos <- cbind(gadm[ ,c(5,7)], geos)
+colnames(geos) <- c("region","district","time","id","observer","lat","lon","BP","CP","WP","rice","cgrid","bloc","BIC")
+
+# Coordinates and number of buildings per quadrat -------------------------
+bp <- geos[which(geos$BP == "Y"), ] ## identify quadrats with buildings
+bp$bloc <- as.character(bp$bloc)
+
+# coordinates of tagged building locations from quadrats with buildings
+c <- fromJSON(bp$bloc[1])
+bcoord <- do.call("rbind", c$feature$geometry$coordinates)
+for(i in 2:nrow(bp)) {
+  c <- fromJSON(bp$bloc[i])
+  bcoord_temp <- do.call("rbind", c$feature$geometry$coordinates)
+  bcoord <- rbind(bcoord, bcoord_temp)
+}
+bcoord <- as.data.frame(bcoord) ## vector of coordinates per quadrats with buildings
+colnames(bcoord) <- c("lon","lat")
+
+# number of tagged building locations from quadrats with buildings
+bcount <- rep(NA, nrow(bp))
+for(i in 1:nrow(bp)) {
+  t <- fromJSON(bp$bloc[i])
+  bcount[i] <- nrow(t$features)
+}
+bcount ## vector of number of buildings per quadrats with buildings
+ba <- geos[which(geos$BP == "N"), ]
+ba$bcount <- 0
+bp <- cbind(bp, bcount)
+geos <- rbind(ba, bp)
+geos <- geos[order(geos$id),] ## sort in original sample order
+
+# project GeoSurvey coords to grid CRS
+geos.proj <- as.data.frame(project(cbind(geos$lon, geos$lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geos.proj) <- c("x","y")
+geos <- cbind(geos, geos.proj)
+coordinates(geos) <- ~x+y
+projection(geos) <- projection(grids)
+
+# extract gridded variables at GeoSurvey locations
+geosgrid <- extract(grids, geos)
+gsdat <- as.data.frame(cbind(geos, geosgrid)) 
+# gsdat <- gsdat[!duplicated(gsdat), ] ## removes any duplicates ... if needed
+gsdat <- gsdat[complete.cases(gsdat[ ,c(16:54)]),] ## removes incomplete cases
+# gsdat <- gsdat[ which(gsdat$CP=='Y'), ] ## selects croplands only
+gsdat$observer <- sub("@.*", "", as.character(gsdat$observer)) ## shortens observer ID's
+
+# Write data frame --------------------------------------------------------
+dir.create("Results", showWarnings = F)
+write.csv(bcoord, "./Results/GH_bcoord.csv", row.names = F)
+write.csv(gsdat, "./Results/GH_gsdat18.csv", row.names = F)
+
+# GeoSurvey map widget ----------------------------------------------------
+w <- leaflet() %>% 
+  addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
+  addCircleMarkers(gsdat$lon, gsdat$lat, clusterOptions = markerClusterOptions())
+w ## plot widget 
+saveWidget(w, 'NG_GS18.html', selfcontained = T) ## save widget
+
+# GeoSurvey contributions -------------------------------------------------
+gscon <- as.data.frame(table(gsdat$observer))
+set.seed(1235813)
+wordcloud(gscon$Var1, freq = gscon$Freq, scale = c(4,0.1), random.order = T)
+
