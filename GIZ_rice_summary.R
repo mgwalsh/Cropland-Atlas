fc79a2ca49d792d6e5e495aa005eb20307ac6043
mgwalsh <markusgwalsh@gmail.com>
2018-06-03 17:18:38 +0300

    Tanzania, Nigeria, Ghana, Ethiopia "paddy rice" 2018 GeoSurvey summary

--- GIZ_rice_summary.R
+++ GIZ_rice_summary.R
@@ -1,14 +1,14 @@
-# Tanzania, Nigeria, Ghana, Ethiopia "paddy rice" GeoSurvey 2018 summary
-# M. Walsh, April 2018
+# Tanzania, Nigeria, Ghana, Ethiopia "paddy rice" 2018 GeoSurvey summary
+# M. Walsh, June 2018
 
 # Required packages
-# install.packages(c("downloader","wordcloud")), dependencies=TRUE)
+# install.packages(c("downloader","MASS")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(downloader)
-  require(wordcloud)
+  require(MASS)
 })
 
-# Data downloads -----------------------------------------------------------
+# Data downloads ----------------------------------------------------------
 # set working directory
 dir.create("Rice_GS18", showWarnings = F)
 setwd("./Rice_GS18")
@@ -17,3 +17,9 @@ setwd("./Rice_GS18")
 download("https://www.dropbox.com/s/ugo0q0u9noy7nyt/GIZ_rice_2018.csv.zip?raw=1", "GIZ_rice_2018.csv.zip", mode = "wb")
 unzip("GIZ_rice_2018.csv.zip", overwrite = T)
 geos <- read.table("GIZ_rice_2018.csv", header = T, sep = ",")
+
+# GS data summary ---------------------------------------------------------
+rice <- geos[which(geos$CP == 'Y'), ]
+test <- glm(rice~Country-1, family = binomial, rice)  
+summary(test)
+
