171fe8a57aa75a20b1516c09613d0d70ab85460c
mgwalsh <markusgwalsh@gmail.com>
2019-01-02 07:03:10 +0300

    Stacked predictions of Nigeria 2018 GeoSurvey settlement observations

--- NG_GS18_pred.R
+++ NG_GS18_pred.R
@@ -243,7 +243,7 @@ write.csv(gsout, "./Results/NG_BP_out.csv", row.names = F) ## ... change feature
 pred <- 1-st.pred ## GeoSurvey ensemble probability
 pal <- colorBin("Reds", domain = 0:1) ## set color palette
 w <- leaflet() %>% 
-  setView(lng = mean(gsdat$lon), lat = mean(gsdat$lat), zoom = 6) %>%
+  setView(lng = mean(gsdat$lon), lat = mean(gsdat$lat), zoom = 7) %>%
   addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
   addRasterImage(pred, colors = pal, opacity = 0.3, maxBytes=6000000) %>%
   addLegend(pal = pal, values = values(pred), title = "Settlement prob.")
