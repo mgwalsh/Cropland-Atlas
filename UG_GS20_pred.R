531be2f728358973c74e9e8999276d14e1319e66
mgwalsh <markusgwalsh@gmail.com>
2020-05-26 05:22:07 +0300

    Stacked predictions of Uganda 2020 GeoSurvey observations

--- UG_GS20_pred.R
+++ UG_GS20_pred.R
@@ -17,7 +17,7 @@ suppressPackageStartupMessages({
 
 # Data setup --------------------------------------------------------------
 rm(list=setdiff(ls(), c("gsdat","grids","glist"))) ## scrub extraneous objects in memory
-gsdat <- gsdat[complete.cases(gsdat[ ,c(12,18:60)]),] ## removes incomplete cases
+gsdat <- gsdat[complete.cases(gsdat[ ,c(10:12,18:63)]),] ## removes incomplete cases
 
 # set calibration/validation set randomization seed
 seed <- 12358
@@ -33,11 +33,11 @@ labs <- c("CP") ## insert other labels (BP,WP ...) here!
 lcal <- as.vector(t(gs_cal[labs]))
 
 # raster calibration features
-fcal <- gs_cal[,18:60]
+fcal <- gs_cal[,18:63]
 
 # Central place theory model <glm> -----------------------------------------
 # select central place covariates
-gf_cpv <- gs_cal[,23:36]
+gf_cpv <- gs_cal[,23:39]
 
 # start doParallel to parallelize model fitting
 mc <- makeCluster(detectCores())
@@ -186,7 +186,7 @@ gspred <- as.data.frame(cbind(gs_val, gspred))
 # stacking model validation labels and features
 gs_val <- as.data.frame(gs_val)
 lval <- as.vector(t(gs_val[labs]))
-fval <- gspred[,61:65] ## subset validation features
+fval <- gspred[,64:68] ## subset validation features
 
 # Model stacking ----------------------------------------------------------
 # start doParallel to parallelize model fitting
